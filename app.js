20a2b3e80369ce42a2529b3e33513753f0c25087
kimjunghun1 <kimjungheun0@gmail.com>
2025-12-04 00:45:05 +0900

    update app.js file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Bye, World!\n');
+  res.end('Hello, World! Everybody\n');
 });
 
 server.listen(port, hostname, () => {
